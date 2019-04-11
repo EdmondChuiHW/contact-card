132bda78a401be6002dcdf58cf1411c536929825
edmond-chui <chui.hinwai@gmail.com>
2019-04-11 13:55:10 -0600

    Updates

---
+++ precache-manifest.f3833418f3b8499dbe319248689be3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contact-card/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "36b49d72bb124a81020b",
+    "url": "/contact-card/static/js/runtime~main.89ce8845.js"
+  },
+  {
+    "revision": "a234cbcd3aad06b1de3c",
+    "url": "/contact-card/static/js/main.2adbdacd.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/contact-card/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a234cbcd3aad06b1de3c",
+    "url": "/contact-card/static/css/main.c8a2745e.chunk.css"
+  },
+  {
+    "revision": "a228f026dbfa6fd3a6dfb3158a928f49",
+    "url": "/contact-card/index.html"
+  }
+];
