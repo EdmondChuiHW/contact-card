da098a2f664259ba8be0306d739708d990fb1ed8
edmond-chui <chui.hinwai@gmail.com>
2019-04-11 21:40:45 -0600

    +start

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Counter } from './Counter';
 import { ResetButton } from './ResetButton';
 import { useCount } from './common/hooks/useCount';
 import { PureServer } from './Server/PureServer';
+import { StartLeftOrRight } from './StartLeftOrRight';
 
 const isLeftServing = ({ startedLeft, totalCount }) => {
-  return startedLeft && (Math.floor(totalCount / 5) % 2 === 0);
+  const isStartingSide = (Math.floor(totalCount / 5) % 2 === 0);
+  return startedLeft ? isStartingSide : !isStartingSide;
 };
 
 const App = () => {
   const [countL, incCountL, resetL] = useCount(0);
   const [countR, incCountR, resetR] = useCount(0);
-  const resetCounts = () => resetL() || resetR();
-  const startedLeft = true;
+  const [startedLeft, setStartedLeft] = useState(undefined);
+  const resetGame = () => resetL() || resetR() || setStartedLeft(undefined);
 
   const totalCount = (countL + countR);
 
   const leftServing = isLeftServing({ startedLeft, totalCount });
 
+  if (startedLeft === undefined) {
+    return <StartLeftOrRight setStartedLeft={setStartedLeft}/>;
+  }
   return (
     <div className="App">
       <div className='server'><PureServer isServing={leftServing} rotateRight={true}/></div>
       <div className='counter'><Counter count={countL} incCount={incCountL}/></div>
       <div className='counter'><Counter count={countR} incCount={incCountR}/></div>
-      <div className='resetButton'><ResetButton onClick={resetCounts}/></div>
+      <div className='resetButton'><ResetButton onClick={resetGame}/></div>
       <div className='server'><PureServer isServing={!leftServing}/></div>
     </div>
   );
---
+++ src/StartLeftOrRight/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { identity } from 'ramda';
+import { ClickArea } from '../ClickArea';
+import './StartLeftOrRight.css';
+
+export const StartLeftOrRight = ({ setStartedLeft = identity }) => <>
+  <div className='StartLeftOrRight'>
+    <ClickArea onClick={() => setStartedLeft(true)}>
+      <div className='button-content'>START LEFT</div>
+    </ClickArea>
+    <ClickArea onClick={() => setStartedLeft(false)}>
+      <div className='button-content'>START RIGHT</div>
+    </ClickArea>
+  </div>
+</>;
